707655759df789d98118568eac9e776e2c6eef52
Tiffany Warner <warnerti@oregonstate.edu>
2019-02-22 11:22:46 -0800

    Added comic book detail view

--- src/Comic-Book/Controllers/ComicBooksController.cs
+++ src/Comic-Book/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace Comic_Book.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller.");
-      
+            return View();
         }
     }
 }
