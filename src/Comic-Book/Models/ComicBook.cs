a99311df2c8320cbb2c33d0072ad45090515c6af
Tiffany Warner <warnerti@oregonstate.edu>
2019-02-22 16:51:58 -0800

    Updated Comic Book Detail View to be strongly typed.

--- src/Comic-Book/Models/ComicBook.cs
+++ src/Comic-Book/Models/ComicBook.cs
@@ -10,7 +10,7 @@ namespace Comic_Book.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string HtmlDescription { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
