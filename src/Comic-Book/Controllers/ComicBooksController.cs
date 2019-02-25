85310c51786d4759d5e6378992e209110d47be9c
Tiffany Warner <warnerti@oregonstate.edu>
2019-02-24 20:23:19 -0800

    Updated Comic Books Controller to use Repository

--- src/Comic-Book/Controllers/ComicBooksController.cs
+++ src/Comic-Book/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using Comic_Book.Models;
+﻿using Comic_Book.Data;
+using Comic_Book.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,24 @@ namespace Comic_Book.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        //Constructor
+        public ComicBooksController()
         {
-            //Using object initializer syntax
-            //Could also just name each property... comicBook.SeriesTitle = "The Amazing Spider-Man";
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+        //? means the type is nullable
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name = "Dan Slott", Role="Script"},
-                    new Artist() {Name = "Humberto Ramos", Role="Pencils"},
-                    new Artist() {Name = "Victor Olazaba", Role="Inks"},
-                    new Artist() {Name = "Edgar Delgado", Role="Colors"},
-                    new Artist() {Name = "Chris Eliopoulos", Role="Letters"}
-                }
-            };
+                return HttpNotFound();
+            }
+            // Because int is a nullable type now, we have to either cast
+            // the id as an int like so: (int) id or we have to call the 
+            // nullable type's value: id.Value. Either method works. 
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
             return View(comicBook);
         }
     }
